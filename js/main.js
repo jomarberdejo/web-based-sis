1b188dfa90a65eb075b6ed13e11eb3f7368a69ae
jomarberdejo <115414791+jomarberdejo@users.noreply.github.com>
2023-06-06 15:59:29 +0800

    Update main.js

--- js/main.js
+++ js/main.js
@@ -40,40 +40,7 @@ function save(event) {
   document.getElementById('form2').reset();
 }
 
-// studentlist.html
 
-function allData() {
-  let table = document.getElementById('table');
-  table.innerHTML = '';
-
-  let studentList = JSON.parse(localStorage.getItem('studentList')) || [];
-
-  studentList.forEach(function (student) {
-    table.innerHTML += `
-      <tr>
-        <td>${student.studentId}</td>
-        <td>${student.fullName}</td>
-        <td>${student.age}</td>
-        <td>${student.gender}</td>
-        <td>${student.email}</td>
-        <td>${student.section}</td>
-        <td>${student.status}</td>
-        <td>
-          <div class= "__actions">
-          <button class="btn btn-sm btn-primary" data-bs-toggle="modal" data-bs-target="#editModal" onclick="openEditModal(${student.id})">
-            <i class= "fa fa-edit"> </i>
-          </button>
-          <button class="btn btn-sm btn-danger" onclick="removeData(${student.id})">
-            <i class="fa fa-trash"></i>
-           
-          </button>
-         </div>
-        </td>
-      </tr>`;
-  });
-     
-}
-    allData();
 function removeData(id) {
   let studentList = JSON.parse(localStorage.getItem('studentList')) || [];
   studentList = studentList.filter(function (student) {

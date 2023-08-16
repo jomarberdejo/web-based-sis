d9afdac63abb39e7e1cc61867c4077b62903c501
jomarberdejo <115414791+jomarberdejo@users.noreply.github.com>
2023-08-16 17:44:38 +0800

    Delete header.js

--- js/header.js
+++
@@ -1,9 +0,0 @@
-const links = document.querySelectorAll('.sidebar nav ul li:not(:last-child) a');
-for (const link of links) {
-  link.addEventListener('click', ()=> {
-    for (const otherLink of links) {
-      otherLink.classList.remove('active');
-    }
-    link.classList.add('active');
-  });
-}
